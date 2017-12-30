52dd1b38df6dcdfbe4748cb87e7b3c9cc4a559e3
johny <jho406@gmail.com>
2017-12-29 20:30:49 -0500

    wip

---
+++ spec/lib/b_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe 'B example' do
+  it "some example" do
+    sleep 1
+    expect(1).to eql 1
+  end
+
+  it "some example again" do
+    sleep 1
+    expect(1).to eql 1
+  end
+end
